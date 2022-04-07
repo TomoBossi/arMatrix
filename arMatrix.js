d557231a6abeb01bc5d5d8cc6ba14225307aa48f
Tomás <tomasbossi97@gmail.com>
2022-04-07 18:50:49 -0300

    Color palette and wheel, free drawing

--- arMatrix.js
+++ arMatrix.js
@@ -9,27 +9,35 @@
 //   - automatically console-log current matrix (done)
 //   - change the matrix dimensions at any point (cut/fill with default depending on current matrix size) (done, but only doable from console)
 //   - infinite undo/redo of moidifications (done)
-//   - pick fill value intuitively
-//   - single pixel value modification (free draw tool)
+//   - pick fill values intuitively (done)
+//   - single pixel value modification (free draw tool (done), boolean mouseOnGUI function to prevent overlapping behaviours)
 //   - multiple pixel value modification (rectangle tool, line tool, circle tool, free draw tool)
 //   - select -> copy/cut -> move tool
+//   - save/load/copy for js/numpy tool
+//   - helper toggleable overlay with brief GUI explanations
+//   - layers
 
-
-
+// Global variables
 // Inner logic
 p5.disableFriendlyErrors = true
 let m     = [] // Matrix
-let mw    = 4 // Matrix width
-let mh    = 4 // Matrix height
+let mw    = 8 // Matrix width
+let mh    = 8 // Matrix height
 let dv    = 0 // Default matrix value
 // let prevm = [] // Previous frame matrix
-let mod   = false // Modified during this frame
+let mod   = false // m was modified during this frame
+let pxChange = false // m was modified on previous frames, while holding LMB
 let mHist = [] // Matrix history
 let cm    = 0  // Current matrix, index for mh
-
-
+let i // General indexing purposes
+let x // General matrix element iteration purposes
+let y // General matrix element iteration purposes
 
 // General visuals
+let xpx1
+let xpx2
+let ypx1
+let ypx2 // Pixel coordinates of corner of matrix element
 let w // Sketch width  (set in setup)
 let h // Sketch height (set in setup)
 let pxd    = 1 // Pixel density 
@@ -52,9 +60,19 @@ let ta     = 0   // Text alpha (dynamic, based on zoom)
 let lineca = [120, 255] // Gridline color and alpha (constant)
 let linea  = 255 // Gridline alpha (dynamic, based on zoom)
 let linew  = 1   // Gridline width
-let valc   = [[125,0,0], [bgc, bgc, bgc]] // Index based value-color array, starting from -1 at index 0
-
-
+let totColors = 30 // Palette size
+let colorsPerRow = 15 // Colors per palette row
+let row = 1 // Palette row index
+let rC // Random color
+let cPalette = [[-1, [222,  82,  82], [222,  82,  82], 0, 0], 
+                [ 0, [bgc, bgc, bgc], [bgc, bgc, bgc], 0, 0],
+                [ 1, [ 40, 200, 100], [ 40, 200, 100], 0, 0],
+                [ 2, [ 62, 152, 218], [ 62, 152, 218], 0, 0],
+                [ 3, [200, 200, 200], [200, 200, 200], 0, 0],
+                [ 4, [177,  88,   6], [177,  88,   6], 0, 0],
+                [ 5, [200, 200,  40], [200, 200,  40], 0, 0],
+                [ 6, [187,  92, 180], [187,  92, 180], 0, 0],
+                [ 7, [100, 100, 100], [100, 100, 100], 0, 0],] // Index based value-color array, [value, cPick, tempPick, buttonx, buttony] (position data set in setup)
 
 // Interactivity
 let prevx // Previous mouseX pos
@@ -62,18 +80,25 @@ let prevy // Previous mouseY pos
 let uipx  = 30 // UI button length
 let uibc  = 80 // UI button color
 let uihc  = 200 // UI highlight color
-let clickButtonArray // Holds all relevant clickable button data in the form [[button1xPos, button1yPos, button1function, button1drawFunction, functionArgs]](set in setup)
+let uipscl = 0.7 // Scale of palette buttons in relation to the rest of the buttons
 let undoredo = true // Undo or Redo was used during the previous frame
 let verbose = true // If true, prints m whenever a change is made to it
-
+let clickButtonArray = [[0, 0, undo, undoButton],
+                        [0, 0, redo, redoButton],
+                        [0, 0, reCenter, reCenterButton],
+                        [0, 0, enlargeMatrix, enlargeMatrixButton, 2],] // Holds all relevant clickable button data in the form [[button1xPos, button1yPos, button1function, button1drawFunction, functionArgs]](set in setup)
 let cPick // Color picked from color wheel
+let cHover // Color under pointer from color wheel
 let cPicking = false // Currently picking a color using the color wheel
-let vMod = false // Currently modifying lightning value on color wheel
-let pxIndex // Index of current pixel 
+let cPickingIndex = null // Index (cPalette) of color being currently picked
+let cSelectIndex = 1 // Index (cPalette) of currently selected color
+let vMod = false // Currently modifying luminosity value on color wheel
+let pxIndex // Index of current pixel while hovering over color wheel
 let v = 0.8 // luminosity value
 let cWx // Color wheel x pos (set in setup)
 let cWy // Color wheel y pos (set in setup)
-let cWd = 100 // Color wheel diameter
+let cWd = uipx*4 // Color wheel diameter
+let onGUI // Mouse pointer currently on GUI elements (palette, tools, etc.)
 
 
 
@@ -88,8 +113,8 @@ function setup() {
     element.addEventListener("contextmenu", (e) => e.preventDefault()) // Prevent context menu popup on right click
   }
   
-  w = windowWidth
-  h = windowHeight
+  w = constrain(windowWidth,  575, windowWidth)
+  h = constrain(windowHeight, 500, windowHeight)
   noSmooth()
   createCanvas(w, h)
   for (let y = 0; y < mh; y++) {
@@ -106,18 +131,35 @@ function setup() {
   vRef = (h-mhpx)/2
   hPan = 0
   vPan = 0
-
-  clickButtonArray = [[w-uipx/1.25, 1*uipx/1.25, undo, undoButton],
-                      [w-uipx/1.25, 3*uipx/1.25, redo, redoButton],
-                      [w-uipx/1.25, 5*uipx/1.25, reCenter, reCenterButton],
-                      [w-uipx/1.25, 7*uipx/1.25, enlargeMatrix, enlargeMatrixButton, 2],]
   
+  i = 1
+  for (let button of clickButtonArray) {
+    button[0] = w-uipx/1.25
+    button[1] = (i*2-1)*uipx/1.25
+    i++
+  }
+  
+  if (totColors > cPalette.length) {
+    for (let i = cPalette[cPalette.length-1][0] + 1; i < totColors-1; i++) {
+      rC = [random(255), random(255), random(255)]
+      cPalette.push([i, [...rC], [...rC], 0, 0])
+    }
+  }
+  
+  i = 1
+  for (let valCol of cPalette) {
+    row = floor((i-1)/colorsPerRow)
+    valCol[3] = (uipx/1.25-uipx/2)+(i*2-1 - row*2*colorsPerRow)*uipx/1.25*uipscl
+    valCol[4] = (uipx/1.25-uipx/2)+(row*2+1)*uipx/1.25*uipscl // uipx/1.25
+    i++
+  }
+
   cWx = cWd/1.725 + uipx/5
-  cWy = cWd/1.725 + 3*uipx/1.25 - uipx/2.25
+  cWy = cWd/1.725 + (1.25+floor(totColors/colorsPerRow))*(uipscl*uipx + uipx/3)
   imageMode(CENTER)
   ellipseMode(CENTER)
   
-  mHist.push([deepCopy2D(m), mw, mh])
+  mHist.push([deepCopy2D(m), mw, mh, getCurrentPalette()])
 }
 
   
@@ -138,14 +180,19 @@ function draw() {
   la   = lineca[1] * zFac**1.5
   // ta   = tca[1] * zFac**2
   
-  // Draw grid 
+  // Draw grid
+  onGUI = false //mouseOnGUI()
   for (let y = 0; y < mh; y++) {
     for (let x = 0; x < mw; x++) {
       
+      xpx1 = hRef + x*s
+      ypx1 = vRef + y*s
+      xpx2 = xpx1 + s
+      ypx2 = ypx1 + s
       stroke(lineca[0], la)
       strokeWeight(linew)
-      fill(valc[m[y][x] + 1])
-      rect(hRef + x*s, vRef + y*s, s, s)
+      fill(cPalette[m[y][x] + 1][2])
+      rect(xpx1, ypx1, s, s)
       
       // fill(tca[0], ta)
       // noStroke()
@@ -153,28 +200,47 @@ function draw() {
       // textSize(s/1.5)
       // text(m[y][x], hRef + x*s+s/2, vRef + y*s+s/2)
       
+      // Free drawing
+      
+      if (mouseIsPressed && mouseButton == LEFT) {
+        if (!onGUI) {
+          if (m[y][x] != cSelectIndex-1) {
+            if (mouseX > xpx1 - s/2 && mouseX < xpx2 - s/2 && mouseY > ypx1 - s/2 && mouseY < ypx2 - s/2) {
+              pxChange = true
+              m[y][x] = cSelectIndex-1
+            } 
+          }
+        }
+      }
     }
   }
   
   // Draw GUI
   drawClickButtons()
-  if (cPicking) {
-    drawColorWheel()
-  }
+  drawColorPalette()
+  drawColorWheel()   // if (cPicking) {} is inside func
+  updateHoverColor() // if (cPicking) {} is inside func
   
   prevx = mouseX
   prevy = mouseY
-  if (!mouseIsPressed && (frameCount == 1 || mod)) {
+  
+  if (frameCount == 1 || mod) {
     if (verbose) {
       console.log(showMatrix(m))
+      // console.log(getCurrentPalette())
     } if (!undoredo) {
-      mHist = mHist.slice(0, cm+1)
-      mHist.push([deepCopy2D(m), mw, mh])
-      cm = cm + 1
+      cm++
+      mHist = mHist.slice(0, cm)
+      mHist.push([deepCopy2D(m), mw, mh, getCurrentPalette()])
     }
+    mod      = false
+    undoredo = false
+    pxChange = false
   }
-  mod = false
-  undoredo = false
+  
+  // for (let matrix of mHist) {
+  //   console.log(showMatrix(matrix[0]))
+  // }
 }
   
 
@@ -320,9 +386,11 @@ function undo() {
   if (cm > 0) {
     cm--
     nm = mHist[cm]
-    m  = nm[0]
+    m  = deepCopy2D(nm[0])
     mw = nm[1]
     mh = nm[2]
+    p  = deepCopy2D(nm[3])
+    setPalette(p)
     // reCenter()
     mod = true
     undoredo = true
@@ -334,10 +402,12 @@ function undo() {
 function redo() {
   if (cm < mHist.length-1) {
     cm++
-    nm  = mHist[cm]
-    m  = nm[0]
+    nm = mHist[cm]
+    m  = deepCopy2D(nm[0])
     mw = nm[1]
     mh = nm[2]
+    p  = deepCopy2D(nm[3])
+    setPalette(p)
     // reCenter()
     mod = true
     undoredo = true
@@ -346,21 +416,19 @@ function redo() {
 
 
 
-function baseButton(x, y) {
+function baseButton(x, y, color) {
   rectMode(CENTER)
   noStroke()
   fill(bgc/2)
-  // ellipse(x + uipx/20, y + uipx/20, uipx)
   rect(x + uipx/10, y + uipx/10, uipx, uipx, uipx/10)
-  fill(uibc)
-  // ellipse(x, y, uipx)
+  fill(color)
   rect(x, y, uipx, uipx, uipx/10)
 }
 
 
 
 function reCenterButton(x, y) {
-  baseButton(x, y)
+  baseButton(x, y, uibc)
   fill(uihc)
   ellipse(x, y, uipx*2.25/3)
   fill(uibc)
@@ -372,7 +440,7 @@ function reCenterButton(x, y) {
 
 
 function enlargeMatrixButton(x, y) {
-  baseButton(x, y)
+  baseButton(x, y, uibc)
   noFill()
   strokeWeight(uipx/20)
   stroke(uihc, 150)
@@ -384,20 +452,7 @@ function enlargeMatrixButton(x, y) {
 
 
 function undoButton(x, y) {
-  baseButton(x, y)
-  fill(uihc)
-  ellipse(x, y, uipx*1.8/3)
-  fill(uibc)
-  ellipse(x, y, uipx*1.4/3)
-  rect(x-uipx/4.5, y-uipx/4.5, uipx/2.25)
-  fill(uihc)
-  triangle(x, y-0.85*uipx/2.25, x, y-0.35*uipx/2.25, x-uipx/6, y-0.6*uipx/2.25)
-}
-
-
-
-function undoButton(x, y) {
-  baseButton(x, y)
+  baseButton(x, y, uibc)
   fill(uihc)
   ellipse(x, y, uipx*1.8/3)
   fill(uibc)
@@ -410,7 +465,7 @@ function undoButton(x, y) {
 
 
 function redoButton(x, y) {
-  baseButton(x, y)
+  baseButton(x, y, uibc)
   fill(uihc)
   ellipse(x, y, uipx*1.8/3)
   fill(uibc)
@@ -433,8 +488,47 @@ function drawClickButtons() {
 
 
 
+function drawColorPalette() {
+  let uipxrscld = uipx*uipscl
+  for (let valCol of cPalette) {
+    let val = valCol[0]
+    let col = valCol[2]
+    let bx  = valCol[3]
+    let by  = valCol[4]
+    let isSelected = cSelectIndex == val+1
+    rectMode(CENTER)
+    noStroke()
+    if (isSelected) {
+      fill(uihc)
+      rect(bx, by, uipxrscld*1.15, uipxrscld*1.15, uipxrscld/10)
+      rect(bx, by+uipxrscld*1.15/1.65, uipxrscld*1.15/2, uipxrscld/15, uipxrscld/15)
+    } else {
+      fill(bgc/2)
+      rect(bx + uipx/10, by + uipx/10, uipxrscld, uipxrscld, uipxrscld/10)
+    }
+    fill(col)
+    rect(bx, by, uipxrscld, uipxrscld, uipxrscld/10)
+    if ((col[0] + col[1] + col[2])/3 < 80) {
+      fill(255, 40)//+80*()
+    } else {
+      fill(0, 50)
+    }
+    rect(bx, by, uipxrscld, uipxrscld, uipxrscld/10)
+    fill(col)
+    rect(bx, by, uipxrscld/1.2, uipxrscld/1.2, uipxrscld/10)
+    stroke(0, 100)
+    strokeWeight(3)
+    fill(255, 200)
+    textAlign(CENTER, CENTER);
+    textSize(uipxrscld/1.9)
+    text(val, bx, by)
+  }
+}
+
+
+
 function mouseClicked() {
-  // Botones
+  // Buttons
   for (let button of clickButtonArray) {
     let bx = button[0]
     let by = button[1]
@@ -447,7 +541,8 @@ function mouseClicked() {
     }
   }
   
-  // Colores
+  // Colors
+  // Wheel
   if (cPicking) {
     if (dist(mouseX, mouseY, cWx, cWy) < cWd/2) {
       pxIndex = (mouseY * width + mouseX) * pxd * 4
@@ -458,6 +553,28 @@ function mouseClicked() {
                pixels[pxIndex + 3]]
       updatePixels()
       cPick = [cPick[0]*v, cPick[1]*v, cPick[2]*v]
+      cPalette[cPickingIndex][1] = [...cPick]
+      cPicking = false
+      cPickingIndex = null
+      mod = true
+    } else if ((mouseX > cWx + cWd*1.15/2 || mouseX < cWx - cWd*1.15/2 || mouseY > cWy + cWd/8 + cWd*1.4/2 || mouseY < cWy + cWd/8 - cWd*1.4/2)) { // Click outside of color wheel window
+      cPicking = false
+      cPickingIndex = null
+    }
+  }
+  // Palette
+  for (let valCol of cPalette) {
+    let bx = valCol[3]
+    let by = valCol[4]
+    let i  = valCol[0] + 1
+    if (mouseX > bx - uipx/2 && mouseX < bx + uipx/2) {
+      if (mouseY > by - uipx/2 && mouseY < by + uipx/2) {
+        if (i == cSelectIndex) {
+          cPickingIndex = i
+          cPicking = true
+        }
+        cSelectIndex = i
+      }
     }
   }
 }
@@ -475,36 +592,120 @@ function deepCopy2D(a) {
 
 
 function mouseReleased() {
+  // Luminosity control
   vMod = false
+  
+  // Drawing
+  if (pxChange) {
+    mod = true
+    pxChange = false
+  }
 }
-
 
 
 function drawColorWheel() {
-  noStroke()
-  rectMode(CENTER)
-  fill(bgc/2)
-  rect(cWx + uipx/10, cWy + cWd/8 + uipx/10, cWd*1.15, cWd*1.4, cWd/8)
-  fill(uibc-30)
-  rect(cWx, cWy + cWd/8, cWd*1.15, cWd*1.4, cWd/8)
-  rectMode(CORNER)
-  fill(uihc)
-  ellipse(cWx, cWy, cWd*21/20)
-  tint(255*v)
-  image(cWheel, cWx, cWy, cWd, cWd)
-  fill(uihc-50)
-  rect(cWx-cWd/2, cWy + cWd/1.55 - cWd/160, cWd*v, cWd/80, cWd/80)
-  fill(uibc)
-  rect(cWx-cWd/2+cWd*v, cWy + cWd/1.55 - cWd/160, cWd*(1-v), cWd/80, cWd/80)
-  fill(uihc)
-  ellipse(cWx-cWd/2+v*cWd, cWy + cWd/1.55, cWd/20)
-  if (mouseIsPressed) {
-    if (vMod || ((mouseX > cWx-cWd/2-cWd/40 && mouseX < cWx+cWd/2+cWd/40) && (mouseY > cWy + cWd/1.8 && mouseY < cWy + cWd/1.3))) {
-      v = constrain(map(mouseX, cWx-cWd/2, cWx+cWd/2, 0, 1), 0, 1)
-      vMod = true
+  if (cPicking) {
+    noStroke()
+    rectMode(CENTER)
+    fill(bgc/2)
+    rect(cWx + uipx/10, cWy + cWd/8 + uipx/10, cWd*1.15, cWd*1.4, cWd/8)
+    fill(uibc-30)
+    rect(cWx, cWy + cWd/8, cWd*1.15, cWd*1.4, cWd/8)
+    rectMode(CORNER)
+    fill(uihc)
+    ellipse(cWx, cWy, cWd*21/20)
+    tint(255*v)
+    image(cWheel, cWx, cWy, cWd, cWd)
+    fill(uihc-50)
+    rect(cWx-cWd/2, cWy + cWd/1.55 - cWd/160, cWd*v, cWd/80, cWd/80)
+    fill(uibc)
+    rect(cWx-cWd/2+cWd*v, cWy + cWd/1.55 - cWd/160, cWd*(1-v), cWd/80, cWd/80)
+    fill(uihc)
+    ellipse(cWx-cWd/2+v*cWd, cWy + cWd/1.55, cWd/20)
+    rectMode(CENTER)
+    if (mouseIsPressed) {
+      if (vMod || ((mouseX > cWx-cWd/2-cWd/40 && mouseX < cWx+cWd/2+cWd/40) && (mouseY > cWy + cWd/1.8 && mouseY < cWy + cWd/1.3))) {
+        v = constrain(map(mouseX, cWx-cWd/2, cWx+cWd/2, 0, 1), 0, 1)
+        vMod = true
+      }
     }
   }
 }
+
+
+
+function updateHoverColor() {
+  if (cPicking) {
+    if (dist(mouseX, mouseY, cWx, cWy) < cWd/2) {
+      pxIndex = (mouseY * width + mouseX) * pxd * 4
+      loadPixels()
+      cHover = [pixels[pxIndex],
+                pixels[pxIndex + 1],
+                pixels[pxIndex + 2],
+                pixels[pxIndex + 3]]
+      updatePixels()
+      cHover = [cHover[0]*v, cHover[1]*v, cHover[2]*v]
+      cPalette[cPickingIndex][2] = cHover
+    } else {
+      cPalette[cPickingIndex][2] = [...cPalette[cPickingIndex][1]]// reset color to cPick
+    }
+  } else if (cPickingIndex) {
+    cPalette[cPickingIndex][2] = [...cPalette[cPickingIndex][1]] // reset color to cPick
+  }
+}
+
+
+
+function getCurrentPalette() {
+  p = []
+  for (let valCol of cPalette) {
+    p.push([...valCol[1]])
+  }
+  return p
+}
+
+
+
+function setPalette(p) {
+  i = 0
+  for (let col of p) {
+    cPalette[i][1] = [...col]
+    cPalette[i][2] = [...col]
+    i++
+  }
+}
+
+
+
+function mouseOnGUI() {
+  let res = false
+  if (mouseIsPressed) {
+    
+  }
+  return res
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
